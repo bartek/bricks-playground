d46e8dd6565d76aae7ff5e73e189b48076255a9c
Bartek Ciszkowski <hello@justbartek.ca>
2020-03-17 21:09:41 -0400

    More changes to index.js

--- src/events/mouse.js
+++ src/events/mouse.js
@@ -1,22 +1,16 @@
 import * as THREE from 'three';
 import { useEffect, useState } from 'react';
 
-export const useMousePosition = (props) => {
-    const { scene, camera, raycaster, mouse } = props;
-    const [position, setPosition] = useState({ x: 0, y: 0, intersect: [] });
+export const useMousePosition = () => {
+    const [position, setPosition] = useState({ x: 0, y: 0, });
     useEffect(() => {
         const setFromEvent = e => {
             let x = (e.clientX / window.innerWidth) * 2 - 1;
             let y = (e.clientY / window.innerHeight) * 2 + 1;
 
-            raycaster.setFromCamera(mouse.clone(), camera);
-
-            let intersect = raycaster.intersectObjects(scene.children);
-
             setPosition({
                 x: x,
                 y: y,
-                intersect: intersect,
             });
         }
         window.addEventListener('mousemove', setFromEvent);
